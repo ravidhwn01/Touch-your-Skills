a58d2b919a25c457dbce3a2270e38b14a9a4aa35
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-01-17 23:01:05 +0530

    header

--- touchyourskills/src/App.js
+++ touchyourskills/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Index from './components/Header/Index'
 function App() {
   return (
     < >
-    
+   <Index/>
     </>
   );
 }
---
+++ touchyourskills/src/components/Header/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <div className="header">
+        <h1> Open job listing </h1>
+
+        <button>Post a job</button>
+      </div>
+    </>
+  );
+};
+
+export default index;
